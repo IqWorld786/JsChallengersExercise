bb40d65a26c75bae67c42b9928c26fe2acb2db73
IqWorld786 <bmiqbalalam0786@gmail.com>
2022-07-15 20:42:08 +0530

    done

---
+++ Javascripts_Basics/extractFirstHalfOfString.js
@@ -0,0 +1,10 @@
+// Write a function that takes a string (a) as argument
+// Extract the first half a
+// Return the result
+
+function myFunction(a) {
+    let half = a.length / 2;
+    return a.slice(0,half);
+ }
+
+ console.log(myFunction('abcdefghijk'));
