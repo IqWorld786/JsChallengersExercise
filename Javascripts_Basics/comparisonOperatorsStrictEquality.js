df80b168bd3ca370573e52a7e4b5c9cad4d7b15e
IqWorld786 <bmiqbalalam0786@gmail.com>
2022-07-15 18:52:48 +0530

    exercise2Done

---
+++ Javascripts_Basics/comparisonOperatorsStrictEquality.js
@@ -0,0 +1,9 @@
+// Write a function that takes two values, say a and b, as arguments
+// Return true if the two values are equal and of the same type
+
+function myFunction(a, b)
+{
+let bool = (a===b);
+return bool;
+}
+ console.log(myFunction('A', "A"));
