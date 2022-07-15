82c381961c56bd5c28c85d249d196895c9cf6356
IqWorld786 <bmiqbalalam0786@gmail.com>
2022-07-15 21:06:57 +0530

    done

---
+++ Javascripts_Basics/bPercentOfA.js
@@ -0,0 +1,9 @@
+// Write a function that takes two numbers (a and b) as argument
+// Return b percent of a
+
+function myFunction(a, b) {
+    let b_pc_a = b/100 * a;
+    return b_pc_a;
+ }
+
+ console.log(myFunction(500,25));
